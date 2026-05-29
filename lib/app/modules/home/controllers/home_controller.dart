1a0767bb7deb278b777ac3a7078054dbf3727587
abhay <abhishekdubey660@gmail.com>
2026-05-29 21:18:16 +0530

    added permission handler code

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:ble_vitals_scanner/app/services/ble_services.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomeController extends GetxController {
   final BleServices _bleServices = BleServices();
@@ -14,18 +15,22 @@ class HomeController extends GetxController {
 
   StreamSubscription<ConnectionStateUpdate>? _connectionSubscription;
 
-  void startScan() {
-    isScanning.value = true;
+  void startScan() async {
+    if (await Permission.bluetoothScan.request().isGranted) {
+      if (await Permission.bluetoothConnect.request().isGranted) {
+        isScanning.value = true;
 
-    _bleServices.startScan(
-      onDeviceFound: (deviceList) {
-        devices.assignAll(deviceList);
-        deviceFound.value = deviceList.length;
-      },
-      onError: (error) {
-        Get.snackbar("Error", error);
-      },
-    );
+        _bleServices.startScan(
+          onDeviceFound: (deviceList) {
+            devices.assignAll(deviceList);
+            deviceFound.value = deviceList.length;
+          },
+          onError: (error) {
+            Get.snackbar("Error", error);
+          },
+        );
+      }
+    }
   }
 
   void stopScan() async {
